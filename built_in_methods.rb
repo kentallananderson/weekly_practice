8c9a4c35a07f26a0c90a29f50539c9717b33a4db
Kent <kentallananderson@gmail.com>
2021-10-09 17:53:45 -0500

    Comment out question 1, use 'if' statment to determine if user entered number converted to integer is greater than 100

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -1,10 +1,16 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
-puts "Enter a word:"
-word = gets.chomp
-puts word.upcase
 
+  # puts "Enter a word:"
+  # word = gets.chomp
+  # puts word.upcase
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Enter a number:"
+number = gets.chomp
+if number.to_i > 100
+  puts "Thats a big number"
+end 
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
