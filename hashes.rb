6670c93a92d772c62c84f9d12ae0b5534196fdf1
Kent <kentallananderson@gmail.com>
2021-10-10 00:30:08 -0500

    Make hash containing 3 menue items. Add 4th item

--- hashes.rb
+++ hashes.rb
@@ -7,16 +7,20 @@
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
 
-  people =[
-    {"first_name" => "John", "last_name" => "Flynn"}, 
-    {"first_name" => "Mike", "last_name" => "Foreman"}, 
-    {"first_name" => "Sarah", "last_name" => "Coder"}
-  ]
+  # people =[
+  #   {"first_name" => "John", "last_name" => "Flynn"}, 
+  #   {"first_name" => "Mike", "last_name" => "Foreman"}, 
+  #   {"first_name" => "Sarah", "last_name" => "Coder"}
+  # ]
 
-  puts people[0]["first_name"]
-  puts people[0]["last_name"]
+  # puts people[0]["first_name"]
+  # puts people[0]["last_name"]
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+  menu_items = {"pizza" => 10, "chiken_strips" => 8, "Fries" => 3.50}
+  menu_items["alfredo"] = 12
+  puts menu_items
 
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
