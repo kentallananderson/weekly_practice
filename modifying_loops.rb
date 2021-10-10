a6b1cd6999a18a290cd2db0389106f32c5b17a58
Kent <kentallananderson@gmail.com>
2021-10-09 21:46:17 -0500

    Create while loop printing 1-10

--- modifying_loops.rb
+++ modifying_loops.rb
@@ -1,5 +1,11 @@
 # 1. Write a while loop to print the numbers 1 through 10.
 
+  i = 1
+  while i <= 10
+    puts i 
+    i += 1
+  end 
+
 # 2. Write a while loop that prints the word "hello" 5 times.
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
