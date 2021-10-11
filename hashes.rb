670bc2af2083cfb40f0dad668fce88244b58be38
Kent <kentallananderson@gmail.com>
2021-10-11 15:57:19 -0500

    Create array of hashes storing book titles and authors, print third author

--- hashes.rb
+++ hashes.rb
@@ -24,13 +24,20 @@
 
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
-  book = {"title" => "The Pearl", "author" => "John Steinbeck", "pages" => 110, "language" => "English"}
-  puts book["title"]
-  puts book["author"]
-  puts book["pages"]
-  puts book["language"]
+  # book = {"title" => "The Pearl", "author" => "John Steinbeck", "pages" => 110, "language" => "English"}
+  # puts book["title"]
+  # puts book["author"]
+  # puts book["pages"]
+  # puts book["language"]
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+  books = [
+    {"title" => "book_1", "author" => "author_1"},
+    {"title" => "book_2", "author" => "author_2"},
+    {"title" => "book_3", "author" => "author_3"}
+  ]
+  puts books[2]["author"]
 
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
